ddf648056d49f4c393e0674ddd70e0cbaffdebf0
ogulcankarayel5 <karayel.ogulcan1903@gmail.com>
2020-01-21 09:11:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms/" component={Rooms} />
+      <Route path="/single-room" component={SingleRoom} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Rooms = () => {
+    return (
+        <div>
+            hello rooms
+        </div>
+    )
+}
+
+export default Rooms
