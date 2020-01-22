6b1bc06c7fee69c7697297ca481ce04b8baaf026
ogulcankarayel5 <karayel.ogulcan1903@gmail.com>
2020-01-22 16:19:15 +0300

    context eklendi

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Navbar from './components/Navbar';
 function App() {
-  return (
+  return ( 
     <>
      <Navbar/>
       <Switch>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
+import FeaturedRooms from '../components/FeaturedRooms';
 export default function Home() {
   return (
     <React.Fragment>
@@ -17,6 +18,7 @@ export default function Home() {
         </Banner>
       </Hero>
       <Services/>
+      <FeaturedRooms/>
     </React.Fragment>
   );
 }
