48856f03ce463926da2c0f05572c8f9fbdf2bae9
ogulcankarayel5 <karayel.ogulcan1903@gmail.com>
2020-01-25 23:50:10 +0300

    sayfalar eklendi

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import {RoomContext} from '../context';
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
+
 export default class FeaturedRooms extends Component {
-    static contextType = RoomContext
-    render() {
-        const {sortedRooms} = this.context;
-        console.log(sortedRooms);
-       
-        return (
-            <div>
-                
-                hello from feautured rooms 
-            </div>
-        )
-    }
+  static contextType = RoomContext;
+  render() {
+    let { featuredRooms, loading } = this.context;
+    console.log(featuredRooms);
+    featuredRooms = featuredRooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
+
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+            {loading ? <Loading/> : featuredRooms}
+        </div>
+        
+      </section>
+    );
+  }
 }
--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ class RoomProvider extends Component {
     featuredRooms: [],
     loading: true
   };
-
+  
   //getData
 
   componentDidMount() {
